9d10dab60da7670f8c80ff14a3cd815bec16918b
Melissa Nuval <mnuval21@gmail.com>
2021-09-01 01:37:11 -0400

    added selected text to populate in body functionality

--- background.js
+++ background.js
@@ -1,20 +1,18 @@
 
 //compose window url
-var baseGmailUrl = "https://mail.google.com/";
-var gmailUrlSuffix = "mail/?view=cm&fs=1&tf=1";
+var baselUrl = "https://mail.google.com/";
+var urlSuffix = "mail/?view=cm&fs=1&tf=1";
 
-function makeGmailDomainUrl() {
-  var gmailUrl = baseGmailUrl;
+function createGmailUrl() {
+  var url = baselUrl;
   var domainName = window.localStorage["domainName"];
   if (domainName) {
-    gmailUrl += "a/" + domainName + "/";
+    url += "a/" + domainName + "/";
   }
-    return gmailUrl + gmailUrlSuffix;
+    return url + urlSuffix;
 }
 
-
-var title = '';
-var url = '';
+//For selected text in body?
 var selectedText = '';
 
 
@@ -23,7 +21,7 @@ chrome.extension.onConnect.addListener(
     if (port.name == "GmailUrlConn") {
       port.onMessage.addListener(function(msg) {
       if (msg.req == "GmailUrlPlease") {
-        port.postMessage({gmailDomainUrl: makeGmailDomainUrl()});
+        port.postMessage({gmailDomainUrl: createGmailUrl()});
       } else {
         console.log("Unsupported req on valid port");
       }
@@ -34,7 +32,7 @@ chrome.extension.onConnect.addListener(
 chrome.extension.onRequest.addListener(
   function(connectionInfo) {
     selectedText = connectionInfo;
-    makeGmailWin(selectedText);
+    gmailWin(selectedText);
 });
 
 
@@ -45,15 +43,21 @@ chrome.browserAction.onClicked.addListener(
     url = tab.url;
 });
 
-function makeGmailWin(summary) {
+function gmailWin(summary) {
   var body = '';
-  var gmailURL = makeGmailDomainUrl() +
+  if (summary == '') {
+    body = url;
+  } else {
+    body = summary + "\n" + url
+    ;
+  }
+  var gmailURL = createGmailUrl() +
                  "&body=" + encodeURIComponent(body);
   chrome.windows.create({
     url: gmailURL,
     left: 20,
     top: 30,
-    width: 700,
-    height: 600
+    width: 600,
+    height: 500
     });
 }
--- browser_email.js
+++ browser_email.js
@@ -1,7 +1,7 @@
 var toField = "&to=";
 var cachedGmailUrl = "";
 
-function rewriteMailtoToGMailUrl(inUrl) {
+function rewriteBrowserEmailToGMailUrl(inUrl) {
   
   var retUrl = inUrl;
   retUrl = retUrl.replace("?", "&");
@@ -15,25 +15,20 @@ function rewriteMailtoToGMailUrl(inUrl) {
 }
 
 // Content Scripts
-function rewriteMailtosOnPage() {
-  console.log("rewriteMailtosOnPage")
-  // Find all the mailto links.
+function rewriteBrowserEmailOnPage() {
   var result = document.evaluate(
       '//a[contains(@href, "mailto:")]',
       document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null);
 
   var item;
   var nodes = [];
-  // cannot change the NODE_ITERATOR nodes' attributes in this loop itself
-  // since iterateNext will invalidate the state; Need to store temporarily.
   while (item = result.iterateNext()) {
     nodes.push(item);
   }
 
   for (var i = 0; i < nodes.length; i++) {
-    console.log("in the for loop mailto.js")
     var mailtoStr = nodes[i].getAttribute('href');
-    mailtoStr = rewriteMailtoToGMailUrl(mailtoStr);
+    mailtoStr = rewriteBrowserEmailToGMailUrl(mailtoStr);
     nodes[i].setAttribute('href', mailtoStr);
     nodes[i].setAttribute('target', "_blank");
   }
@@ -45,8 +40,8 @@ if (cachedGmailUrl == "") {
   bgPort.onMessage.addListener(
     function(msg) {
       cachedGmailUrl = msg.gmailDomainUrl;
-      rewriteMailtosOnPage();
+      rewriteBrowserEmailOnPage();
     });
 } else {
-  rewriteMailtosOnPage();
+  rewriteBrowserEmailOnPage();
 }
